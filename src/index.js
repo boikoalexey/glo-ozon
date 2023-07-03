327cb2d68e9cab1d2b3f938ad540b3273ced5b19
Alexey Boyko <alexeyboiko@bk.ru>
2023-07-03 16:06:03 +0300

    search

--- src/index.js
+++ src/index.js
@@ -75,6 +75,10 @@ const addingGoodsToCart = () => {
 const filterAndSearchOnPage = () => {
     const cards = document.querySelectorAll('.goods .card');
     const discountCheckbox = document.getElementById('discount-checkbox');
+    const min = document.getElementById('min');
+    const max = document.getElementById('max');
+    const search = document.querySelector('.search-wrapper_input');
+    const searchBtn = document.querySelector('.search-btn');
 
     discountCheckbox.addEventListener('change', () => {
         cards.forEach(card => {
@@ -87,6 +91,34 @@ const filterAndSearchOnPage = () => {
             }
         });
     });
+
+    const filterPrice = () => {
+        cards.forEach(card => {
+            const cardPrice = card.querySelector('.card-price');
+            const price = parseFloat(cardPrice.textContent);
+            if ((min.value && price < min.value) || (max.value && price > max.value)) {
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = 'block';
+            }
+        });
+    };
+
+    min.addEventListener('change', filterPrice);
+    max.addEventListener('change', filterPrice);
+
+    searchBtn.addEventListener('click', () => {
+        const searchText = new RegExp(search.value.trim(), 'i');
+        cards.forEach(card => {
+            const title = card.querySelector('.card-title');
+            if (!searchText.test(title.textContent)) {
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = 'block';
+            }
+        });
+    });
+
 };
 
 toggleCheckbox();
